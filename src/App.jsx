f7dd0cc82c4d352755a8b432d01dcdcd5abd57c9
Manoj Khati <mksea18@gmail.com>
2024-07-20 00:15:37 +0545

    seteffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 // import Contactform from "./Contactform";
 // import Survey from "./Survey";
 // import StateHook from "./StateHook";
-import StateHookArray from "./StateHookArray";
+// import StateHookArray from "./StateHookArray";
+import Timer from "./SetEffect";
 const App = () => {
   return (
     <>
       {/* <Survey /> */}
       {/* <StateHook /> */}
-      <StateHookArray />
+      {/* <StateHookArray /> */}
+      <Timer />
     </>
   );
 };
--- src/StateHookArray.jsx
+++ src/StateHookArray.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState, Button } from "react";
 const StateHookArray = () => {
   const [data, setData] = useState([
     { name: "anoj" },
     { name: "manoj" },
     { name: "sanoj" },
   ]);
-  const handleChange = (e, index) => {};
+  const handleChange = () => {};
   return (
     <div>
       <ul>
         {data.length > 0 &&
-          data.map((item) => {
-            <li>{item.name}</li>;
-            <input placeholder="enter correct name" />;
-            onChange = {};
-          })}
-        ;
+          data.map((item, index) => (
+            // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
+
+            <li key={index}>
+              <span>{item.name}</span>
+              {""}
+            </li>
+          ))}
+        <Button onClick={handleChange} />;
       </ul>
     </div>
   );
